645a63a9602af7f125d1bc95be6f0ecd88d5ee40
iamdebadipti <iamdebadipti@gmail.com>
2020-10-04 13:28:04 +0530

    🚀 login & regiter mutation working

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,37 @@
-import { Link } from '@reach/router';
-import React from 'react';
+import { Link, useNavigate } from '@reach/router';
+import React, { useState } from 'react';
+import { useLoginMutation } from '../../generated/graphql';
 import styles from './index.module.scss';
 
 interface Props {}
 
 const Login: React.FC<Props> = () => {
+  const navigate = useNavigate();
+  const [{ email, password }, setInput] = useState({ email: '', password: '' });
+  const [loginMutation, { data, loading }] = useLoginMutation();
+
+  // login mutation
+  const handleLogin = async (event: any) => {
+    event.preventDefault();
+    try {
+      await loginMutation({ variables: { email, password } });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // input change handler
+  const handleInputChange = (event: any) => {
+    event.persist();
+    setInput((state) => ({ ...state, [event.target.name]: event.target.value }));
+  };
+
+  if (data?.login.id) {
+    navigate('/', { state: { authenticated: true } }); // successful login --> move to '/' route
+  }
+
+  // console.log(data);
+
   return (
     <div className={styles.auth_wrap}>
       <div className={styles.auth_box}>
@@ -12,17 +39,31 @@ const Login: React.FC<Props> = () => {
         <p>
           Don’t have an account? <Link to="/register">Register Now</Link>
         </p>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="input_group">
             <label htmlFor="email">Email Address</label>
-            <input type="email" name="email" id="email" placeholder="Enter your email address" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+              onChange={handleInputChange}
+            />
           </div>
           <div className="input_group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Enter your password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              onChange={handleInputChange}
+            />
           </div>
           <div className="input_submit">
-            <button type="submit">Log in</button>
+            <button type="submit" disabled={loading}>
+              Log in
+            </button>
             <Link to="/forgot-password">Forgot Password?</Link>
           </div>
         </form>
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,10 +1,32 @@
 import { Link } from '@reach/router';
-import React from 'react';
+import React, { useState } from 'react';
+import { useRegisterMutation } from '../../generated/graphql';
 import styles from './index.module.scss';
 
 interface Props {}
 
 const Register: React.FC<Props> = () => {
+  const [{ name, email, password }, setInput] = useState({ name: '', email: '', password: '' });
+  const [registerMutation, { data, loading }] = useRegisterMutation();
+
+  const handleRegister = async (event: any) => {
+    event.preventDefault();
+    try {
+      await registerMutation({ variables: { name, email, password } });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (event: any) => {
+    event.persist();
+    setInput((state) => ({ ...state, [event.target.name]: event.target.value }));
+  };
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div className={styles.auth_wrap}>
       <div className={styles.auth_box}>
@@ -12,21 +34,35 @@ const Register: React.FC<Props> = () => {
         <p>
           Already have an account? <Link to="/login">Log In</Link>
         </p>
-        <form>
+        <form onSubmit={handleRegister}>
           <div className="input_group">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Enter your full name" />
+            <input type="text" name="name" id="name" placeholder="Enter your full name" onChange={handleInputChange} />
           </div>
           <div className="input_group">
             <label htmlFor="email">Email Address</label>
-            <input type="email" name="email" id="email" placeholder="Enter your email address" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+              onChange={handleInputChange}
+            />
           </div>
           <div className="input_group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Enter your password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              onChange={handleInputChange}
+            />
           </div>
           <div className="input_submit">
-            <button type="submit">Create</button>
+            <button type="submit" disabled={loading}>
+              Create
+            </button>
           </div>
         </form>
       </div>
