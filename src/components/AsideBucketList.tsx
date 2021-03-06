c062448671c1c67c4a8e09527d32baee3e7263dd
iamdebadipti <iamdebadipti@gmail.com>
2021-03-07 00:07:04 +0530

    added real bucket list data

--- src/components/AsideBucketList.tsx
+++ src/components/AsideBucketList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { useAllBucketsQuery } from '../generated/graphql';
+import { useAllBucketsQuery } from '../generated/graphql';
 import AsideBucketItem from './AsideBucketItem';
 import styles from './AsideBucketList.module.scss';
 
@@ -7,33 +7,29 @@ interface Props {
   selectedBucket: string | null;
 }
 
-// dummy data for testing
-const dummy = [
-  { id: 'd68s76f7s6f8sd7f', name: '🚀 Getting Started', count: '3', color: '#14AAF5', completion: '33' },
-  { id: '76d87y8s7dfs7yfs', name: '🗳 Noted React Native', count: '12', color: '#ED5353', completion: '50' },
-  { id: '768asda7sd6sdasd', name: '⛸ Ice Skate Practice', count: '8', color: '#692FC2', completion: '0' },
-  { id: 'uihasd8as89879as', name: '🏖 Travel Plan 2021', count: '6', color: '#FFAA00', completion: '66' }
-];
-
 const AsideBucketList: React.FC<Props> = ({ selectedBucket }) => {
-  // const { data } = useAllBucketsQuery();
-  // console.log(data);
+  const { data, loading, error } = useAllBucketsQuery();
+
+  if (error) return <div>error</div>;
 
   return (
     <div className={styles.bucket}>
       <h3>My Buckets</h3>
       <div className={styles.bucket_list}>
-        {dummy.map((bucket) => (
-          <AsideBucketItem
-            key={bucket.id}
-            id={bucket.id}
-            label={bucket.name}
-            count={bucket.count}
-            selected={bucket.id === selectedBucket}
-            color={bucket.color}
-            completion={bucket.completion}
-          />
-        ))}
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          data?.allBuckets.map((bucket) => (
+            <AsideBucketItem
+              key={bucket.id}
+              id={bucket.id}
+              label={bucket.name}
+              count={bucket.tasks?.length}
+              selected={bucket.id === selectedBucket}
+              color={bucket.color}
+            />
+          ))
+        )}
       </div>
     </div>
   );
