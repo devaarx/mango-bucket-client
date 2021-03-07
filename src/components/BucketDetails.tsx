70035a4c2094e9e39c1b40ae2124333dd8bb169f
iamdebadipti <iamdebadipti@gmail.com>
2021-03-07 22:13:33 +0530

    bucket details changes

--- src/components/BucketDetails.tsx
+++ src/components/BucketDetails.tsx
@@ -16,16 +16,24 @@ const BucketDetails: React.FC<Props> = ({ bucketId }) => {
   return (
     <div className={styles.details}>
       <BucketDescription bucketTitle={data?.bucketInfo.name} bucketDescription={data?.bucketInfo?.description} />
-      {data?.bucketInfo.tasks?.map((bucket) => (
-        <TaskItem
-          key={bucket.id}
-          id={bucket.id}
-          title={bucket.name}
-          status={bucket.status}
-          label={bucket.status}
-          onChange={() => console.log('object')}
-        />
-      ))}
+      <div className={styles.section}>
+        <h3 className={styles.section_heading}>Tasks</h3>
+        <div className={styles.section_content}>
+          {data?.bucketInfo.tasks?.length ? (
+            data?.bucketInfo.tasks?.map((bucket) => (
+              <TaskItem
+                key={bucket.id}
+                id={bucket.id}
+                title={bucket.name}
+                status={bucket.status}
+                onChange={() => console.log('object')}
+              />
+            ))
+          ) : (
+            <p>No Tasks</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
